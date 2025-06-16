c75e3899c6e568a9879422ad4a431b28349d238b
Gurur Kısaoğlu <kisaoglugurur@gmail.com>
2025-06-16 04:11:58 +0300

    Initial Commit

---
+++ TwistedRPS/TwistedRPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwistedRPSApp.swift
+//  TwistedRPS
+//
+//  Created by Gurur on 16.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct TwistedRPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
